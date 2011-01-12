e406b22534fa627cec55ee5c1ba33f47b20c018a
caefer <caefer@ical.ly>
2011-01-12 18:15:36 +0100

    Removed a debuggin artefact from test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,20 @@
 <?php
+/**
+ * This file is part of the ImageTransform package.
+ * (c) Christian Schaefer <caefer@ical.ly>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 require_once __DIR__.'/../src/vendor/Symfony/Component/HttpFoundation/UniversalClassLoader.php';
 
 use Symfony\Component\HttpFoundation\UniversalClassLoader;
 
-echo __DIR__.'/../src/ImageTransform'.PHP_EOL;
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
   'ImageTransform' => __DIR__.'/../src',
   'Symfony' => __DIR__.'/../src/vendor'
 ));
+
 $loader->register();
