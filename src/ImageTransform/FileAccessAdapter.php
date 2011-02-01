13565c396c6eb9ecec153d1ab34836fd66efafcd
caefer <caefer@ical.ly>
2011-02-01 10:01:38 +0100

    Implemented loading and dumping delegated from the Image and not the transformer

---
+++ src/ImageTransform/FileAccessAdapter.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the ImageTransform package.
+ * (c) Christian Schaefer <caefer@ical.ly>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ImageTransform;
+
+/**
+ * Interface defining file access for image resources
+ *
+ * @author Christian Schaefer <caefer@ical.ly>
+ */
+interface FileAccessAdapter
+{
+  /**
+   * Create an image resource
+   *
+   * @param ImageTransform\Image $image  Instance to create a resource for
+   * @param integer              $width  Width of the image to be created
+   * @param integer              $height Height of the image to be created
+   */
+  public function create(ImageTransform\Image $image, $width, $height);
+
+  /**
+   * Open a resource for an Image
+   *
+   * @param ImageTransform\Image $image    Instance to create a resource for
+   * @param string               $filepath Location of the file to open
+   */
+  public function open(ImageTransform\Image $image, $filepath);
+
+  /**
+   * Flush an Image resource to stdout
+   *
+   * @param ImageTransform\Image $image  Instance to create a resource for
+   * @param string               $mimeType Mime type of the target file
+   */
+  public function flush(ImageTransform\Image $image, $mimeType = false);
+
+  /**
+   * Save an Image resource under its current location
+   *
+   * @param ImageTransform\Image $image  Instance to create a resource for
+   */
+  public function save(ImageTransform\Image $image);
+
+  /**
+   * Save an Image resource under a given filepath
+   *
+   * @param ImageTransform\Image $image    Instance to create a resource for
+   * @param string               $filepath Locastion where to save the resource
+   * @param string               $mimeType Mime type of the target file
+   */
+  public function saveAs(ImageTransform\Image $image, $filepath, $mimeType = false);
+}
--- src/ImageTransform/Image.php
+++ src/ImageTransform/Image.php
@@ -9,8 +9,6 @@
 
 namespace ImageTransform;
 
-use ImageTransform\Image\Exception\DelegateNotFoundException;
-
 /**
  * Image class.
  *
@@ -21,19 +19,26 @@ use ImageTransform\Image\Exception\DelegateNotFoundException;
 class Image
 {
   /**
+   * @var ImageTransform\FileAccessAdapter $adapter
+   */
+  protected static $adapter;
+
+  /**
    * @var array $attributes Key / value store to be used for meta information by delegates.
    */
-  protected $attributes = array(
-  );
+  protected $attributes = array();
 
   /**
    * C'tor
    *
    * @param array $classNames List of Delegate inheriting classes providing transformations.
    */
-  public function __construct($filepath = null)
+  public function __construct($filepath = false)
   {
-    $this->set('source.filepath', $filepath);
+    if(false !== $filepath)
+    {
+      $this->open($this, $filepath);
+    }
   }
 
   /**
@@ -51,11 +56,45 @@ class Image
   /**
    * Attribute mutator.
    *
-   * @param  string $key   Name of the attribute to set
-   * @param  array  $value Value to be set
+   * @param string $key   Name of the attribute to set
+   * @param array  $value Value to be set
    */
   public function set($key, $value)
   {
     $this->attributes[$key] = $value;
+  }
+
+  /**
+   * Dispatches calls to self::$adapter
+   *
+   * @param  string $method Name of method called
+   * @param  array  $arguments Arguments passed to this call
+   * @return ImageTransform\Image
+   */
+  public function __call($method, $arguments)
+  {
+    if(!(self::$adapter instanceof ImageTransform\FileAccessAdapter))
+    {
+      throw new \BadMethodCallException($method.'() can not be called as no adapter is set!');
+    }
+
+    if(is_callable(array(self::$adapter, $method)))
+    {
+      throw new \BadMethodCallException($method.'() is not implemented!');
+    }
+
+    call_user_func_array(array(self::$adapter, $method), $arguments);
+
+    return $this;
+  }
+
+  /**
+   * Registers adapter for file access
+   *
+   * @param ImageTransform\FileAccessAdapter $adapter Instance implementing ImageTransform\FileAccessAdapter interface
+   */
+  public static function setFileAccessAdapter(\ImageTransform\FileAccessAdapter $adapter)
+  {
+    self::$adapter = $adapter;
   }
 }
